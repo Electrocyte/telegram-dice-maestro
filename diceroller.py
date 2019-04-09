0c0c9f59d12ed4c84ea8103174ae0953e0f8de4a
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-04-09 20:22:13 +0800

    Format code

--- diceroller.py
+++ diceroller.py
@@ -11,14 +11,13 @@ RESET_KEY = "RESET"
 PLUS_KEY = "PLUS"
 MINUS_KEY = "MINUS"
 
-NUMBERS = [
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-]
+NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 NUMBER_STATE = 0
 SIDES_STATE = 1
 MODIFIER_STATE = 2
 DONE_STATE = 3
+
 
 class DiceRollerBuilder:
     def __init__(self):
@@ -59,7 +58,6 @@ class DiceRollerBuilder:
             elif input == ENTER_KEY:
                 self.state = DONE_STATE
 
-
     def reply_text(self):
         if self.state < DONE_STATE:
             number = self.print_number()
@@ -87,7 +85,6 @@ class DiceRollerBuilder:
             roll = roll_dice(number, sides, modifier)
             return create_roll_response(roll)
 
-
     def print_number(self):
         if self.number:
             return self.number
@@ -106,15 +103,13 @@ class DiceRollerBuilder:
         else:
             return ""
 
-
     def build_menu(self, buttons, n_cols, header_buttons=None, footer_buttons=None):
-        menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+        menu = [buttons[i : i + n_cols] for i in range(0, len(buttons), n_cols)]
         if header_buttons:
             menu.insert(0, header_buttons)
         if footer_buttons:
             menu.append(footer_buttons)
         return menu
-
 
     def button_gen(self):
         if self.state == DONE_STATE:
@@ -123,21 +118,22 @@ class DiceRollerBuilder:
             InlineKeyboardButton("↻", callback_data=RESET_KEY),
             InlineKeyboardButton("←", callback_data=BACKSPACE_KEY),
             InlineKeyboardButton("↵", callback_data=ENTER_KEY),
-            InlineKeyboardButton("1", callback_data='1'),
-            InlineKeyboardButton("2", callback_data='2'),
-            InlineKeyboardButton("3", callback_data='3'),
-            InlineKeyboardButton("4", callback_data='4'),
-            InlineKeyboardButton("5", callback_data='5'),
-            InlineKeyboardButton("6", callback_data='6'),
-            InlineKeyboardButton("7", callback_data='7'),
-            InlineKeyboardButton("8", callback_data='8'),
-            InlineKeyboardButton("9", callback_data='9'),
+            InlineKeyboardButton("1", callback_data="1"),
+            InlineKeyboardButton("2", callback_data="2"),
+            InlineKeyboardButton("3", callback_data="3"),
+            InlineKeyboardButton("4", callback_data="4"),
+            InlineKeyboardButton("5", callback_data="5"),
+            InlineKeyboardButton("6", callback_data="6"),
+            InlineKeyboardButton("7", callback_data="7"),
+            InlineKeyboardButton("8", callback_data="8"),
+            InlineKeyboardButton("9", callback_data="9"),
             InlineKeyboardButton("+", callback_data=PLUS_KEY),
-            InlineKeyboardButton("0", callback_data='0'),
+            InlineKeyboardButton("0", callback_data="0"),
             InlineKeyboardButton("-", callback_data=MINUS_KEY),
         ]
         reply_markup = InlineKeyboardMarkup(self.build_menu(button_list, n_cols=3))
         return reply_markup
+
 
 def parse_command(arg):
     if "+" in arg:
@@ -145,7 +141,7 @@ def parse_command(arg):
         no_of_dice, no_dice_sides = remainder.split("d")
     elif "-" in arg:
         remainder, modifier = arg.split("-")
-        modifier = int(modifier)*-1
+        modifier = int(modifier) * -1
         no_of_dice, no_dice_sides = remainder.split("d")
     else:
         no_of_dice, no_dice_sides = arg.split("d")
@@ -156,6 +152,7 @@ def parse_command(arg):
     modifier = int(modifier)
 
     return no_of_dice, no_dice_sides, modifier
+
 
 def roll_dice(no_of_dice, no_dice_sides, modifier):
     # Limit number of dice roll to something sane
@@ -171,10 +168,10 @@ def roll_dice(no_of_dice, no_dice_sides, modifier):
         modifier_text = ""
 
     value = {
-        'parameters': f"{no_of_dice}d{no_dice_sides}{modifier_text}",
-        'rolls': rolls,
-        'modifier': modifier_text,
-        'sum': sum(rolls) + modifier
+        "parameters": f"{no_of_dice}d{no_dice_sides}{modifier_text}",
+        "rolls": rolls,
+        "modifier": modifier_text,
+        "sum": sum(rolls) + modifier,
     }
 
     return value
@@ -190,28 +187,32 @@ def unknown(update: Update, _context: CallbackContext):
 
 def create_roll_response(roll):
     """Craft a message to the user after rolling the dice"""
-    list_rolls = " + ".join([str(i) for i in roll.get('rolls')])
-    parameters = roll['parameters']
-    modifier = roll['modifier']
+    list_rolls = " + ".join([str(i) for i in roll.get("rolls")])
+    parameters = roll["parameters"]
+    modifier = roll["modifier"]
     sums = str(roll.get("sum"))
     return f"Parameters: {parameters}\nRoll: ({list_rolls}) {modifier} \nYour final roll is: 🎲 {sums} 🎲 "
 
 
 def roll_command(update: Update, context: CallbackContext):
-#   update.message.reply_text(args)
+    #   update.message.reply_text(args)
     args = context.args
     if len(args) == 0:
         builder = DiceRollerBuilder()
         context.chat_data["builder"] = builder
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text=builder.reply_text(),
-                                 reply_markup=builder.button_gen())
+        context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text=builder.reply_text(),
+            reply_markup=builder.button_gen(),
+        )
 
     for arg in args:
         try:
             no_of_dice, no_dice_sides, modifier = parse_command(arg)
         except ValueError as err:
-            update.message.reply_markdown(f"‼️ Your input '{arg}' was invalid: {err}‼️\n\n💣 Roll dice in the format '1d20+5' for example; no spaces 💣 ")
+            update.message.reply_markdown(
+                f"‼️ Your input '{arg}' was invalid: {err}‼️\n\n💣 Roll dice in the format '1d20+5' for example; no spaces 💣 "
+            )
             continue
 
         roll = roll_dice(no_of_dice, no_dice_sides, modifier)
@@ -226,44 +227,46 @@ def callback(update: Update, context: CallbackContext):
     bot = context.bot
     DDR = update.callback_query
     try:
-        builder = context.chat_data['builder']
+        builder = context.chat_data["builder"]
     except KeyError:
         builder = DiceRollerBuilder()
-        context.chat_data['builder'] = builder
+        context.chat_data["builder"] = builder
     builder.new_input(DDR.data)
 
     reply_text = builder.reply_text()
 
     if reply_text != DDR.message.text:
-        bot.edit_message_text(chat_id = DDR.message.chat.id,
-                              message_id = DDR.message.message_id,
-                              reply_markup = builder.button_gen(),
-                              text = reply_text)
+        bot.edit_message_text(
+            chat_id=DDR.message.chat.id,
+            message_id=DDR.message.message_id,
+            reply_markup=builder.button_gen(),
+            text=reply_text,
+        )
     else:
-        bot.answer_callback_query(callback_query_id = DDR.id,
-                                  text = "That did not work. Try again.",
-                                  show_alert = True)
-
+        bot.answer_callback_query(
+            callback_query_id=DDR.id, text="That did not work. Try again.", show_alert=True
+        )
 
 
 def make_bot(token):
-    #bot = telegram.Bot(token=token)
-    #print(bot.get_me())
+    # bot = telegram.Bot(token=token)
+    # print(bot.get_me())
     updater = Updater(token=token, use_context=True)
 
     ##For quicker access to the Dispatcher used by your Updater, you can introduce it locally:
     dispatcher = updater.dispatcher
 
     ##This is a good time to set up the logging module, so you will know when (and why) things don't work as expected:
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    )
 
-
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler("start", start)
     dispatcher.add_handler(start_handler)
 
-#    testing ...
-#    echo_handler = MessageHandler(Filters.text, echo)
-#    dispatcher.add_handler(echo_handler)
+    #    testing ...
+    #    echo_handler = MessageHandler(Filters.text, echo)
+    #    dispatcher.add_handler(echo_handler)
 
     roll_handler = CommandHandler("roll", roll_command)
     dispatcher.add_handler(roll_handler)
@@ -302,8 +305,8 @@ def get_token():
     if token:
         return token
 
-    token_file = os.getenv("TELEGRAM_TOKEN_FILE", ".telegram-token"):
-    with open(token_file, 'r') as file:
+    token_file = os.getenv("TELEGRAM_TOKEN_FILE", ".telegram-token")
+    with open(token_file, "r") as file:
         return file.read().strip()
 
 
