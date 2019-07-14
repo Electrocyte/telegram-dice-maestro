e8f19725adb75127d87e46f02f6380a3c01f7e13
James Strutt <10740291+Electrocyte@users.noreply.github.com>
2019-07-14 18:32:52 +0800

    chi square test (incomplete)

--- yeenoghu_randomness_checker.py
+++ yeenoghu_randomness_checker.py
@@ -5,15 +5,42 @@ Created on Sun Jul 14 17:36:13 2019
 @author: Mangifera
 """
 
-with open("yeenoghu.txt", "r") as text_file:
-    demon = text_file.read()
+import seaborn as sns
+import pandas as pd
+from scipy import stats
+
+
+
+def is_it_random(filename):
+    with open(filename, "r") as text_file:
+        demon = text_file.read()
+        
+    demon = [int(x) for x in demon.split('\n')]
     
-demon = [int(x) for x in demon.split('\n')]
+    occurrence = {}
+    
+    for i in demon:
+        if i in occurrence:
+            occurrence[i] += 1
+        else:
+            occurrence[i] = 1
+    
+    occurrences = pd.DataFrame.from_dict(occurrence, orient = "index", columns=['rolls'])
+    occurrences = occurrences.reset_index()
+    occurrences = occurrences.rename(index=str, columns={"index": "die_side"})
 
-occurrence = {}
+    sns.set(style="whitegrid")
+    ax = sns.barplot(x="die_side", y="rolls", data=occurrences)
+    
+#    chi2 = stats.chi2_contingency(occurrences["rolls"])
+#    
+#    chi_square_stat = chi2[0]
+#    p_value = chi2[1]
+#    degrees_of_freedom = chi2[2]
+#    
+#    print (chi_square_stat, p_value, degrees_of_freedom)
 
-for i in demon:
-    if i in occurrence:
-        occurrence[i] += 1
-    else:
-        occurrence[i] = 1
+
+#is_it_random("yeenoghu.txt")
+is_it_random("actual_data_yeenoghu.txt")
+
